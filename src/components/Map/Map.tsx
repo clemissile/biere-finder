fce122e281def881e4bd470163361f3a32589a2f
clemissile <ganivetclement@gmail.com>
2024-09-03 20:36:08 +0200

    :bento: new marker

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { LatLngExpression } from "leaflet";
+import { Icon, LatLngExpression } from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
@@ -8,6 +8,13 @@ type MapProps = {
   bars: Bar[] | null;
   setClickedBar: (bar: Bar | null) => void;
 };
+
+const foodIcon = new Icon({
+  iconUrl: "https://img.icons8.com/color/48/beer.png",
+  iconSize: [35, 35],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
 
 export const Map = (props: MapProps) => {
   const { bars, setClickedBar } = props;
@@ -31,6 +38,7 @@ export const Map = (props: MapProps) => {
             <Marker
               key={bar.id}
               position={bar.latlon}
+              icon={foodIcon}
               eventHandlers={{
                 click: () => {
                   setClickedBar(bar);
